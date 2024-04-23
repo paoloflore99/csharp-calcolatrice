441bfd6f2d6f209b5d82792e7dc644d88af44489
paolo flore <paoloflore937@gmail.com>
2024-04-23 15:14:51 +0200

    omma di due numeri interi - Somma di due numeri double - Differenza tra due numeri interi

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    internal static class CalcoliHelper
+    {
+
+        public static int ValoreUnoInt;
+        public static int ValoreDueInt;
+
+        public static double ValoreDoubleUNo;
+        public static double ValoreDoubleDue;
+
+
+
+        public static int SommaInt(int ValoreUnoInt, int ValoredueInt)
+        {
+            return ValoreUnoInt + ValoredueInt;
+        }
+
+        public static double SommaDouble(double ValoreDoubleUNo, double ValoreDoubleDue)
+        {
+            return ValoreDoubleUNo + ValoreDoubleDue;
+        }
+
+
+
+
+        public static int DifferenzeINt(int ValoreUnoInt, int ValoredueInt)
+        {
+            return ValoreUnoInt - ValoredueInt;
+        }
+
+        public static double DifferenzeDouble(double ValoreDoubleUNo, double ValoreDoubleDue)
+        {
+            return ValoreDoubleUNo - ValoreDoubleDue;
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+             static void stampa()
+            {
+                Console.Write($" ");
+            }
+
+
+
+            Console.WriteLine("cosa vuoi fare?");
+            Console.WriteLine("1 per addizioni int");
+            Console.WriteLine("2 per addizioni double");
+            string scelta = Console.ReadLine();
+            if (scelta == "1")
+            {
+
+                stampa();
+                int ValoreUnoInt = Convert.ToInt32(Console.ReadLine());
+                Console.Write("+");
+                stampa();
+                int ValoreDueInt = Convert.ToInt32(Console.ReadLine());
+
+                int somma = CalcoliHelper.SommaInt(ValoreUnoInt, ValoreDueInt); ;
+                Console.Write($" = {somma}");
+            } 
+            else if (scelta == "2")
+            {
+                stampa();
+                double ValoreDoubleUNo = Convert.ToDouble(Console.ReadLine());
+                Console.Write("+");
+                stampa();
+                double ValoreDoubleDue = Convert.ToDouble(Console.ReadLine());
+                double sommaDouble = CalcoliHelper.SommaDouble(ValoreDoubleUNo, ValoreDoubleDue);
+                Console.Write($" = {sommaDouble}");
+            }
+            else if (scelta == "3")
+            {
+                stampa();
+                int ValoreUnoInt = Convert.ToInt32(Console.ReadLine());
+                Console.Write("-");
+                int ValoredueInt = Convert.ToInt32(Console.ReadLine());
+                int DifferenzeINt = CalcoliHelper.DifferenzeINt(ValoreUnoInt, ValoredueInt);
+                Console.WriteLine($" = {DifferenzeINt}");
+
+
+            }
         }
     }
 }
